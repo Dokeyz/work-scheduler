e253a46357a51b1967c061926c213a4d589b944b
Dokeyz <rikens1996@gmail.com>
2022-07-14 22:12:05 -0400

    fixed color blocks

--- docs/script.js
+++ docs/script.js
@@ -1,16 +1,33 @@
 //current date to display under the header
-
+var $colorBlocks = $(".row");
 var currentDate = moment().format('dddd, MMMM Do');
 $("#currentDay").text(currentDate);
+//hour format taken from the display tab in docs
+var Hour = moment().format('H');
+//save information on click to local storage
+$(".saveBtn").on("click"), function() {
+    var text = $(this).siblings(".description").val();
+    var time = $(this).parent().attr("id");
 
+    localStorage.setItem(time, text);
+}
+//function for assigning colors based on the time
+function Colors(){
+    $colorBlocks.each(function(){
+      var $colorBlock = $(this);
+      //convert hour string into hour number
+      var rowcolor = parseInt($colorBlock.attr("data-hour"));
 
-//button
-
-/*var date = moment();
-moment().format('YYYY-MM-DD');
-console.log(moment.format('YYYY-MM-DD'));
-function today () {
-    document.getElementById("currentDay").innerHTML = "Today's Date: " + date;
+      if (rowcolor == Hour) {
+        $colorBlock.addClass("present").removeClass("past future");
+      }
+      if (rowcolor < Hour) {
+        $colorBlock.addClass("past").removeClass("present future");
+      }
+      if (rowcolor > Hour) {
+        $colorBlock.addClass("future").removeClass("past present");
+      }
+    });
 }
 
-today();*/
+Colors();
