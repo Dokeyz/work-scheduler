ff5449bc357707223af8e10d28b700b55a73b218
Dokeyz <rikens1996@gmail.com>
2022-07-14 22:39:50 -0400

    fixed formatting issue of button by removing pre-wrap from row style

--- docs/script.js
+++ docs/script.js
@@ -4,30 +4,30 @@ var currentDate = moment().format('dddd, MMMM Do');
 $("#currentDay").text(currentDate);
 //hour format taken from the display tab in docs
 var Hour = moment().format('H');
-//save information on click to local storage
-$(".saveBtn").on("click"), function() {
-    var text = $(this).siblings(".description").val();
-    var time = $(this).parent().attr("id");
-
+//save information on click to local storage on button click
+$(".saveBtn").on("click", function() {
+    var text = $(this).siblings(".textarea").val();
+    var time = $(this).parent().attr("hour");
     localStorage.setItem(time, text);
-}
+});
 //function for assigning colors based on the time
 function Colors(){
     $colorBlocks.each(function(){
       var $colorBlock = $(this);
       //convert hour string into hour number
-      var rowcolor = parseInt($colorBlock.attr("data-hour"));
-
+      var rowcolor = parseInt($colorBlock.attr("hour"));
+        //sets hour block to future (green)
       if (rowcolor == Hour) {
         $colorBlock.addClass("present").removeClass("past future");
       }
+      //sets hour block to present (red)
       if (rowcolor < Hour) {
         $colorBlock.addClass("past").removeClass("present future");
       }
+      //sets hour block to past (gray)
       if (rowcolor > Hour) {
         $colorBlock.addClass("future").removeClass("past present");
       }
     });
 }
-
 Colors();
