93eda7c6d9cb9d03f13a887fe251053f9736f539
Dokeyz <rikens1996@gmail.com>
2022-07-14 22:54:24 -0400

    Finalized code, simplified javascript

--- docs/script.js
+++ docs/script.js
@@ -1,33 +1,35 @@
 //current date to display under the header
-var $colorBlocks = $(".row");
+var rows = $(".row");
 var currentDate = moment().format('dddd, MMMM Do');
 $("#currentDay").text(currentDate);
 //hour format taken from the display tab in docs
 var Hour = moment().format('H');
 //save information on click to local storage on button click
-$(".saveBtn").on("click", function() {
+$(".saveBtn").on("click", function () {
     var text = $(this).siblings(".textarea").val();
     var time = $(this).parent().attr("hour");
     localStorage.setItem(time, text);
 });
-//function for assigning colors based on the time
-function Colors(){
-    $colorBlocks.each(function(){
-      var $colorBlock = $(this);
-      //convert hour string into hour number
-      var rowcolor = parseInt($colorBlock.attr("hour"));
+//function for initializing the rows based on the time
+function initialize() {
+    rows.each(function () {
+        var row = $(this);
+        //convert hour string into hour number
+        var rowhour = parseInt(row.attr("hour"));
         //sets hour block to future (green)
-      if (rowcolor == Hour) {
-        $colorBlock.addClass("present").removeClass("past future");
-      }
-      //sets hour block to present (red)
-      if (rowcolor < Hour) {
-        $colorBlock.addClass("past").removeClass("present future");
-      }
-      //sets hour block to past (gray)
-      if (rowcolor > Hour) {
-        $colorBlock.addClass("future").removeClass("past present");
-      }
+        if (rowhour == Hour) {
+            row.addClass("present").removeClass("past future");
+        }
+        //sets hour block to present (red)
+        if (rowhour < Hour) {
+            row.addClass("past").removeClass("present future");
+        }
+        //sets hour block to past (gray)
+        if (rowhour > Hour) {
+            row.addClass("future").removeClass("past present");
+        }
+        //grab the todo from localstorage
+        row.children(".textarea").val(localStorage.getItem(rowhour));
     });
 }
-Colors();
+initialize();
